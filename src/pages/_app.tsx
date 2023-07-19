4726c208064b0ee0716ef23cb4977fca12a6b628
Luiz Prosdoskimi <luizprosdoskimi@gmail.com>
2023-07-19 19:51:59 -0300

    dando inicio a criação do carousel e sua aplicabilidade

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -3,13 +3,14 @@ import { globalStyle } from "../styles/global";
 
 import logoImg from "../assets/logo.svg";
 import { Container, Header } from "../styles/pages/app";
+import Image from "next/image";
 
 globalStyle();
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <Container>
       <Header>
-        <img src={logoImg.src} alt="logo" />
+        <Image src={logoImg} alt="logo" />
       </Header>
       <Component {...pageProps} />
     </Container>
