9861753b4424db779417457af2bf3ed05bbfde95
Luiz Prosdoskimi <luizprosdoskimi@gmail.com>
2023-07-20 18:36:25 -0300

    criando o carousel e o metodo statico para popular os produtos vindo da api do stripe

--- src/styles/global.ts
+++ src/styles/global.ts
@@ -4,12 +4,16 @@ export const globalStyle = globalCss({
   "*": {
     margin: 0,
     padding: 0,
+    boxSizing: "border-box",
   },
 
   body: {
     backgroundColor: "$gray900",
-    "--webkit-font-smoothing": "antialiased",
+    color: "$gray100",
+
+    "-webkit-font-smoothing": "antialiased",
   },
+
   "body, input, textarea, button": {
     fontFamily: "Roboto",
     fontWeight: 400,
