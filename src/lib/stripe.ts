e1abe1cf5d095b09fa8737fe06495b2b630fa3b8
Luiz Prosdoskimi <luizprosdoskimi@gmail.com>
2023-12-07 19:19:08 -0300

    env to deploy

--- src/lib/stripe.ts
+++ src/lib/stripe.ts
@@ -1,8 +1,16 @@
 import Stripe from "stripe";
 
-const environment: string = process.env.STRIPE_SECRET_KEY ?? ''
+let environment: string = process.env.STRIPE_SECRET_KEY ?? ''
 
-export const stripe = new Stripe(environment, {
+export const getEnvironmentVariable = (env:string) => {
+  if(process.env.STRIPE_SECRET_KEY) {
+    return environment
+  } else{
+    throw new Error(`${env} environment variable is not set`)
+  }
+}
+
+export const stripe = new Stripe(getEnvironmentVariable(environment), {
   apiVersion: "2022-11-15",
   appInfo: {
     name: "Ignite Shop",
