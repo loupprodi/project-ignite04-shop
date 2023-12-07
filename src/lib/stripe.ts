543b86a9d06d1fa1ae8d71407b593a88f3169f6c
Luiz Prosdoskimi <luizprosdoskimi@gmail.com>
2023-12-07 19:12:22 -0300

    fixing environment .env key

--- src/lib/stripe.ts
+++ src/lib/stripe.ts
@@ -1,6 +1,8 @@
 import Stripe from "stripe";
 
-export const stripe = new Stripe(process.env.STRIPE_SECRET_KEY!, {
+const environment: string = process.env.STRIPE_SECRET_KEY ?? ''
+
+export const stripe = new Stripe(environment, {
   apiVersion: "2022-11-15",
   appInfo: {
     name: "Ignite Shop",
